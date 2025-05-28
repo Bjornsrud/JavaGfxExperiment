b13a954ae73a3865e876f83127b794e105efd3ed
Bjornsrud <ckb78@outlook.com>
2025-05-29 00:01:35 +0200

    Add class for 3d position.

---
+++ src/main/java/Position3D.java
@@ -0,0 +1,9 @@
+public class Position3D {
+    public double x, y, z;
+
+    public Position3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
