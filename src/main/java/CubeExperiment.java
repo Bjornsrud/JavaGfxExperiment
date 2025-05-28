b3d810fa4af42e143b46ecaa8ed3e7317a754e74
Bjornsrud <ckb78@outlook.com>
2025-05-28 23:48:23 +0200

    Add grid to help with perspective.

--- src/main/java/CubeExperiment.java
+++ src/main/java/CubeExperiment.java
@@ -9,6 +9,7 @@ public class CubeExperiment extends JPanel {
         setBackground(Color.BLACK);
         Graphics2D g2 = (Graphics2D) g;
 
+        drawGrid(g2, -1, 7, 7);
 
         drawPlaneZ(-1, g2, new Color(0, 0, 200), 3, 3);   // Bakplan
         drawPlaneZ(0, g2, new Color(0, 200, 0), 3, 3);     // Midtplan
@@ -35,6 +36,40 @@ public class CubeExperiment extends JPanel {
                 g2.fillOval(x2d - radius / 2, y2d - radius / 2, radius, radius);
             }
         }
+    }
+
+    private void drawGrid(Graphics2D g2, double z, int xCount, int yCount) {
+        g2.setColor(new Color(50, 50, 50));
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+        double scale = (z + 3) / 3.0;
+
+        int xStart = -xCount / 2;
+        int yStart = -yCount / 2;
+
+        for (int i = 0; i < xCount; i++) {
+            int xi = xStart + i;
+            int x = (int) (centerX + xi * scale * 80);
+            int yTop = (int) (centerY + yStart * scale * 80);
+            int yBottom = (int) (centerY + (yStart + yCount - 1) * scale * 80);
+            g2.drawLine(x, yTop, x, yBottom); // vertikale
+        }
+
+        for (int j = 0; j < yCount; j++) {
+            int yi = yStart + j;
+            int y = (int) (centerY + yi * scale * 80);
+            int xLeft = (int) (centerX + xStart * scale * 80);
+            int xRight = (int) (centerX + (xStart + xCount - 1) * scale * 80);
+            g2.drawLine(xLeft, y, xRight, y); // horisontale
+        }
+
+        // Ramme
+        int topLeftX = (int)(centerX + xStart * scale * 80);
+        int topLeftY = (int)(centerY + yStart * scale * 80);
+        int sizeX = (int)(scale * 80 * (xCount - 1));
+        int sizeY = (int)(scale * 80 * (yCount - 1));
+        g2.drawRect(topLeftX, topLeftY, sizeX, sizeY);
     }
 
 
