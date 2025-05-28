f049f1785e5d1f650ee62143992db70f1235b03a
Bjornsrud <ckb78@outlook.com>
2025-05-28 23:17:09 +0200

    Draw a single point that will be center of cube.

---
+++ src/main/java/CubeExperiment.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CubeExperiment extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        setBackground(Color.BLACK);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(new Color(200, 200, 0)); // Yellow
+
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+
+        // Ett enkelt punkt i senter av kuben
+        double x = 0;
+        double y = 0;
+        double z = 0;
+
+        double scale = (z + 3) / 3.0;
+        int x2d = (int) (centerX + x * scale * 80);
+        int y2d = (int) (centerY + y * scale * 80);
+        int radius = (int) (scale * 10);
+        g2.fillOval(x2d - radius / 2, y2d - radius / 2, radius, radius);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CubeExperiment");
+        CubeExperiment panel = new CubeExperiment();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
